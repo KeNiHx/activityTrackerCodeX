a4dc466d4e7dbc83f646c096208bb57d9bb4b3af
leepalisoc <leepalisoc@me.com>
2018-11-16 12:28:09 -0700

    My comment too.

--- Controllers/RegisterViewController.swift
+++ Controllers/RegisterViewController.swift
@@ -20,5 +20,8 @@ class RegisterViewController: UIViewController {
     // MARK: Function for making sure the texts in the description label goes to top-left and not middle when screen size goes bigger
     override func viewDidLayoutSubviews() {
         lblDescription.sizeToFit()
+        
+        
+        /// My comment too.
     }
 }
