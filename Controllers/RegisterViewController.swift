d19ffdb89e774fce808747b8688c6c8f7a43e0b3
leepalisoc <leepalisoc@me.com>
2018-11-16 20:38:50 -0700

    Added more keyboard events for accessibility.

--- Controllers/RegisterViewController.swift
+++ Controllers/RegisterViewController.swift
@@ -25,54 +25,87 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
     // MARK: Start listening for keyboard hide/show events
     private func addKeyboardListeners() {
         // Keyboard Listeners
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardChanges(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardChanges(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardChanges(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     // MARK: Stop listening for keyboard hide/show events
     deinit {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
     // MARK: Keyboard events
-    // Frame goes up a little bit for small screens.
-    @objc func keyboardChanges(notification: Notification) {
-        view.frame.origin.y = -80
-    }
     
     // Event for pressing Return key
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == txtUsername {
             // When Return key is pressed, the focus goes to the next field
-            textField.resignFirstResponder()
             txtEmailAddress.becomeFirstResponder()
         }
         if textField == txtEmailAddress {
             // When Return key is pressed, the focus goes to the next field
-            textField.resignFirstResponder()
             txtPassword.becomeFirstResponder()
         }
         if textField == txtPassword {
             // When Return key is pressed, the focus goes to the next field
-            textField.resignFirstResponder()
             txtConfirmPassword.becomeFirstResponder()
         }
         if textField == txtConfirmPassword {
-            // When Return key is pressed while in password field, the frame goes back to normal and signs in the user
+            // When Return key is pressed while in password field, keyboard will hide.
             textField.resignFirstResponder()
-            view.frame.origin.y = 0
         }
         return true
     }
     
+    // MARK: Functions for moving the frame when keyboard shows/hides
+    // Showing keyboard
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if self.view.frame.origin.y == 0 {
+            self.view.frame.origin.y -= 150
+        }
+    }
+    
+    // Hiding keyboard
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    // MARK: Setting up Delegates, Tap Gestures
     private func setupUI() {
         // Adding delegates
         txtUsername.delegate = self
         txtEmailAddress.delegate = self
         txtPassword.delegate = self
         txtConfirmPassword.delegate = self
+        
+        // Tap Gesture: For when the user taps outside the keyboard, the keyboard dismisses
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        
+        // Adding "Done" button on the keyboard
+        // init the toolbar
+        let toolbar:UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 30))
+        
+        // create empty space on the left side so that "done" button is set on the right side
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        // init the "Done" button with its name and action
+        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(WelcomeViewController.doneButtonAction))
+        
+        toolbar.setItems([flexSpace, doneBtn], animated: false)
+        toolbar.sizeToFit()
+        
+        // Setting the toolbar as inputAccessoryView for every element that needs it
+        self.txtUsername.inputAccessoryView = toolbar
+        self.txtEmailAddress.inputAccessoryView = toolbar
+        self.txtPassword.inputAccessoryView = toolbar
+        self.txtConfirmPassword.inputAccessoryView = toolbar
+    }
+    
+    // MARK: Action for "Done" button on the keyboard.
+    @objc func doneButtonAction() {
+        self.view.endEditing(true)
     }
 }
--- Controllers/WelcomeViewController.swift
+++ Controllers/WelcomeViewController.swift
@@ -27,41 +27,45 @@ class WelcomeViewController: UIViewController, UITextFieldDelegate {
     // MARK: Start listening for keyboard hide/show events
     private func addKeyboardListeners() {
         // Keyboard Listeners
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardChanges(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardChanges(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardChanges(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     // MARK: Stop listening for keyboard hide/show events
     deinit {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-    }
-    
-    // MARK: Keyboard events
-    // Frame goes up a little bit for small screens.
-    @objc func keyboardChanges(notification: Notification) {
-        view.frame.origin.y = -100
     }
     
     // Event for pressing Return key
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == txtUsername {
             // When Return key is pressed, the focus goes to the next field
-            textField.resignFirstResponder()
             txtPassword.becomeFirstResponder()
         }
         if textField == txtPassword {
             // When Return key is pressed while in password field, the frame goes back to normal and signs in the user
-            textField.resignFirstResponder()
             txtPassword.resignFirstResponder()
-            view.frame.origin.y = 0
             
             // Check user first if it exists or not
             checkUserExistence()
         }
         return true
+    }
+    
+    // MARK: Functions for moving the frame when keyboard shows/hides
+    // Showing keyboard
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if self.view.frame.origin.y == 0 {
+            self.view.frame.origin.y -= 100
+        }
+    }
+    
+    // Hiding keyboard
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
     
     // MARK: Listener for making sure password is more than 4 characters
@@ -79,7 +83,7 @@ class WelcomeViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    // MARK: Setting Up UI Design
+    // MARK: Setting Up UI, Adding Delegates, Tap Gestures
     private func setupUI() {
         // Adding delegates
         txtUsername.delegate = self
@@ -92,6 +96,31 @@ class WelcomeViewController: UIViewController, UITextFieldDelegate {
         btnContinue.layer.shadowOpacity = 0.2
         btnContinue.layer.shadowOffset = CGSize(width: 1, height: 2)
         btnContinue.layer.shadowRadius = 15
+        
+        // Tap Gesture: For when the user taps outside the keyboard, the keyboard dismisses
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        
+        // Adding "Done" button on the keyboard
+        // init the toolbar
+        let toolbar:UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 30))
+        
+        // create empty space on the left side so that "done" button is set on the right side
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        // init the "Done" button with its name and action
+        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(WelcomeViewController.doneButtonAction))
+        
+        toolbar.setItems([flexSpace, doneBtn], animated: false)
+        toolbar.sizeToFit()
+        
+        // Setting the toolbar as inputAccessoryView for every element that needs it
+        self.txtUsername.inputAccessoryView = toolbar
+        self.txtPassword.inputAccessoryView = toolbar
+    }
+    
+    // MARK: Action for "Done" button on the keyboard.
+    @objc func doneButtonAction() {
+        self.view.endEditing(true)
     }
     
     // MARK: Checking user's account
