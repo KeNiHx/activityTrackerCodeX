f2f95ee49f225436dca95c1b8aed74de21b773e2
leepalisoc <leepalisoc@me.com>
2018-11-16 17:27:15 -0700

    Updated Register UI Design.

---
+++ Controllers/RegisterPageViewController.swift
@@ -0,0 +1,77 @@
+//
+//  RegisterViewController.swift
+//  Move
+//
+//  Created by Lee Palisoc on 2018-11-15.
+//  Copyright © 2018 CodeX. All rights reserved.
+//
+
+import UIKit
+
+class RegisterPageViewController: UIPageViewController {
+    
+    fileprivate lazy var pages: [UIViewController] = {
+        return [
+            self.getViewController(withIdentifier: "RequiredInfo2BoardID"),
+            self.getViewController(withIdentifier: "OptionalInfoBoardID"),
+            self.getViewController(withIdentifier: "OptionalInfo2BoardID")
+        ]
+    }()
+    
+    fileprivate func getViewController(withIdentifier identifier: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        self.delegate = self
+        
+        if let firstVC = pages.first
+        {
+            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+    }
+}
+
+extension RegisterPageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = pages.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard pages.count > previousIndex else {
+            return nil
+        }
+        
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = pages.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < pages.count else {
+            return nil
+        }
+        
+        guard pages.count > nextIndex else {
+            return nil
+        }
+        
+        return pages[nextIndex]
+    }
+}
+
+extension RegisterPageViewController: UIPageViewControllerDelegate { }
--- Controllers/RegisterViewController.swift
+++ Controllers/RegisterViewController.swift
@@ -2,77 +2,77 @@
 //  RegisterViewController.swift
 //  Move
 //
-//  Created by Lee Palisoc on 2018-11-15.
+//  Created by Lee Palisoc on 2018-11-16.
 //  Copyright © 2018 CodeX. All rights reserved.
 //
 
 import UIKit
 
-class RegisterPageViewController: UIPageViewController {
-    
-    fileprivate lazy var pages: [UIViewController] = {
-        return [
-            self.getViewController(withIdentifier: "RequiredInfoBoardID"),
-            self.getViewController(withIdentifier: "RequiredInfo2BoardID"),
-            self.getViewController(withIdentifier: "OptionalInfoBoardID"),
-            self.getViewController(withIdentifier: "OptionalInfo2BoardID")
-        ]
-    }()
-    
-    fileprivate func getViewController(withIdentifier identifier: String) -> UIViewController {
-        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier)
-    }
+class RegisterViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtEmailAddress: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    @IBOutlet weak var btnContinue: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.dataSource = self
-        self.delegate = self
-        
-        if let firstVC = pages.first
-        {
-            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
-        }
-    }
-}
-
-extension RegisterPageViewController: UIPageViewControllerDataSource {
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        
-        guard let viewControllerIndex = pages.index(of: viewController) else {
-            return nil
-        }
-        
-        let previousIndex = viewControllerIndex - 1
-        
-        guard previousIndex >= 0 else {
-            return nil
-        }
-        
-        guard pages.count > previousIndex else {
-            return nil
-        }
-        
-        return pages[previousIndex]
+        setupUI()
+        addKeyboardListeners()
     }
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
-    {
-        guard let viewControllerIndex = pages.index(of: viewController) else {
-            return nil
+    // MARK: Start listening for keyboard hide/show events
+    private func addKeyboardListeners() {
+        // Keyboard Listeners
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardChanges(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardChanges(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardChanges(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    // MARK: Stop listening for keyboard hide/show events
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    // MARK: Keyboard events
+    // Frame goes up a little bit for small screens.
+    @objc func keyboardChanges(notification: Notification) {
+        view.frame.origin.y = -80
+    }
+    
+    // Event for pressing Return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == txtUsername {
+            // When Return key is pressed, the focus goes to the next field
+            textField.resignFirstResponder()
+            txtEmailAddress.becomeFirstResponder()
         }
-        
-        let nextIndex = viewControllerIndex + 1
-        
-        guard nextIndex < pages.count else {
-            return nil
+        if textField == txtEmailAddress {
+            // When Return key is pressed, the focus goes to the next field
+            textField.resignFirstResponder()
+            txtPassword.becomeFirstResponder()
         }
-        
-        guard pages.count > nextIndex else {
-            return nil
+        if textField == txtPassword {
+            // When Return key is pressed, the focus goes to the next field
+            textField.resignFirstResponder()
+            txtConfirmPassword.becomeFirstResponder()
         }
-        
-        return pages[nextIndex]
+        if textField == txtConfirmPassword {
+            // When Return key is pressed while in password field, the frame goes back to normal and signs in the user
+            textField.resignFirstResponder()
+            view.frame.origin.y = 0
+        }
+        return true
+    }
+    
+    private func setupUI() {
+        // Adding delegates
+        txtUsername.delegate = self
+        txtEmailAddress.delegate = self
+        txtPassword.delegate = self
+        txtConfirmPassword.delegate = self
     }
 }
-
-extension RegisterPageViewController: UIPageViewControllerDelegate { }
