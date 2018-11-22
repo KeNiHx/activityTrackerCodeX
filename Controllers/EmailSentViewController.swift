47586d36637378441c6f8cf1d6aa62c93cbbecb3
Kenny <klam1231hi@gmail.com>
2018-11-21 21:11:25 -0700

    Added email verification

---
+++ Controllers/EmailSentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmailSentViewController.swift
+//  Move
+//
+//  Created by Kenny Lam on 2018-11-21.
+//  Copyright © 2018 CodeX. All rights reserved.
+//
+
+import UIKit
+
+class EmailSentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
