16574fd9f6aa578b0db825ed4a95d62a5bee57a1
leepalisoc <leepalisoc@me.com>
2018-11-25 13:22:51 -0700

    Added a workout session controller.

---
+++ Controllers/WorkoutOutdoorSessionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WorkoutOutdoorSessionViewController.swift
+//  Move
+//
+//  Created by Lee Palisoc on 2018-11-25.
+//  Copyright © 2018 CodeX. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutOutdoorSessionViewController: UIViewController {
+
+    /**
+     @IBOutlets
+     @brief Outlets needed for this current controller.
+     */
+    @IBOutlet weak var lblWorkoutType: UILabel!
+    @IBOutlet weak var btnStartWorkout: UIButton!
+    @IBOutlet weak var viewSettingGoals: UIView!
+    @IBOutlet weak var viewWorkoutDetails: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - Function for setting up the UI
+    private func setupUI() {
+        // Adding shadows on the floating views
+        viewWorkoutDetails.layer.shadowOpacity = 0.5
+        viewWorkoutDetails.layer.shadowRadius = 15
+        viewWorkoutDetails.layer.shadowOffset = CGSize(width: 1, height: 2)
+        viewWorkoutDetails.layer.cornerRadius = 8
+    
+        lblWorkoutType.layer.shadowRadius = 8
+        lblWorkoutType.layer.shadowOpacity = 0.3
+        lblWorkoutType.layer.shadowOffset = CGSize(width: 1, height: 2)
+    }
+}
